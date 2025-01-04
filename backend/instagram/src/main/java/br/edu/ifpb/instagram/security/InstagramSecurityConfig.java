13ebebe010de15cac402ff3c69fbccf80ff5a006
Paulo Pereira <ppereira.dev@gmail.com>
2025-01-03 23:48:42 -0300

    feat: add jwt util token generation

--- backend/instagram/src/main/java/br/edu/ifpb/instagram/security/InstagramSecurityConfig.java
+++ backend/instagram/src/main/java/br/edu/ifpb/instagram/security/InstagramSecurityConfig.java
@@ -1,8 +1,16 @@
 package br.edu.ifpb.instagram.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -10,13 +18,36 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class InstagramSecurityConfig {
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http
-			.csrf((csrf) -> csrf.disable());
-
-		return http.build();
+		return http.csrf(customizer -> customizer.disable()).
+                authorizeHttpRequests(request -> request
+                        .requestMatchers("auth/login").permitAll()
+                        .anyRequest().authenticated()).
+                httpBasic(Customizer.withDefaults()).
+                sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
 	}
+
+
+     @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(userDetailsService);
+
+        return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+
+    }
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
