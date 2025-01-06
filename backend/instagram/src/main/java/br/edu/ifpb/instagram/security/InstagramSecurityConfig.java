532ea2a1e30a4089314e59da91fc0bbf0503369a
Paulo Pereira <ppereira.dev@gmail.com>
2025-01-06 01:18:30 -0300

    fix: add the authenticationprovider to the securityconfig class because it is used in the security filter chain

--- backend/instagram/src/main/java/br/edu/ifpb/instagram/security/InstagramSecurityConfig.java
+++ backend/instagram/src/main/java/br/edu/ifpb/instagram/security/InstagramSecurityConfig.java
@@ -38,6 +38,14 @@ public class InstagramSecurityConfig {
 	}
 
     @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
 
