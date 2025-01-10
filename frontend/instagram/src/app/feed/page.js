ded22f2a0c74ad6e23dc17634f91884f31171096
Paulo Pereira <ppereira.dev@gmail.com>
2025-01-10 11:24:58 -0300

    chore: update the feed page with some funny messages

--- frontend/instagram/src/app/feed/page.js
+++ frontend/instagram/src/app/feed/page.js
@@ -33,7 +33,7 @@ export default function Feed() {
   return (
     <div className="grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 items-center">
-        <h1 className="text-3xl">GOOD JOB! 👍</h1>
+        <h1 className="text-3xl">Você está logado.</h1>
         <Image
           src={
             "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExM3hhcHVrMXBjbGxhNmd6NGIxejB2Y3U3cmRhbjAxMXA5OTB6YzV0eCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/26FmRMFqoOcs18t9e/giphy.webp"
@@ -44,9 +44,9 @@ export default function Feed() {
           alt={`friends`}
           unoptimized={true}
         />
-        <h1 className="text-3xl">Você está logado.</h1>
+        <h1 className="text-3xl">GOOD JOB! 👍</h1>
         <h1 className="text-1xl">
-          Aqui era para ser o feed, quem sabe no futuro. 👷⚙️
+          Aqui será o feed, devs working. 👷⚙️
         </h1>
         {/* Botão de Logout */}
         <button
