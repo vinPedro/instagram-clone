4637f527f2943330d4343e959606e785c9fade0a
Matheus Rafael <matheusrafael2005@gmail.com>
2025-08-17 04:57:32 -0300

    test: add tests for generateToken and getUsernameFromToken for JwtUtils

---
+++ backend/instagram/src/test/java/br/edu/ifpb/instagram/security/JwtUtilsTest.java
@@ -0,0 +1,49 @@
+package br.edu.ifpb.instagram.security;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.security.core.Authentication;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+public class JwtUtilsTest {
+
+    private JwtUtils jwtUtils;
+    private Authentication authentication;
+
+    @BeforeEach
+    void setUp() {
+        jwtUtils = new JwtUtils();
+        authentication = Mockito.mock(Authentication.class);
+    }
+
+    @Test
+    void testGenerateToken_ShouldReturnValidJwt_WhenAuthenticationIsValid() {
+        String username = "matheus";
+        when(authentication.getName()).thenReturn(username);
+        String token = jwtUtils.generateToken(authentication);
+        assertNotNull(token);
+        assertFalse(token.isEmpty());
+        assertTrue(jwtUtils.validateToken(token));
+    }
+
+    @Test
+    void testGetUsernameFromToken_ShouldReturnUsername_WhenTokenIsValid() {
+        String expectedUsername = "matheus";
+        when(authentication.getName()).thenReturn(expectedUsername);
+        String token = jwtUtils.generateToken(authentication);
+        String actualUsername = jwtUtils.getUsernameFromToken(token);
+        assertNotNull(actualUsername);
+        assertEquals(expectedUsername, actualUsername);
+    }
+
+    @Test
+    void testGetUsernameFromToken_ShouldThrowException_WhenTokenIsInvalid() {
+        String invalidToken = "token invalid test";
+        assertThrows(RuntimeException.class, () -> {
+            jwtUtils.getUsernameFromToken(invalidToken);
+        });
+    }
+}
